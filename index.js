39d99ba15d79fc2fee823c02663380cacbcde64a
SupremeERG <ethang717163@icloud.com>
2020-06-18 19:36:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,6 @@ bot.on('message' , async msg=>{
     if(msg.content.toLowerCase() === 'e!verify' && msg.channel.id === '714428169691725915')
     {
         const role = msg.guild.roles.cache.get('697343291825455124');
-        let verifyCH = (msg.guild.channels.cache.get('723153779297615953'))
         if (role) {
             msg.delete();
             msg.member.roles.add(role);
@@ -102,12 +101,7 @@ bot.on('message' , async msg=>{
             .setThumbnail(msg.author.displayAvatarURL())
             .setColor(0x119e32);
             msg.author.send(Vembed);
-            if(verifyCH) {
-                msg.channel.send('ok')
-            }
-            //if(msg.guild.channels.cache.find(x => x.id == '723153779297615953')) {
-                //msg.channel.send(msg.author.tag + ' was succesfully verified!');
-            //}
+            bot.channels.cache.get('723153779297615953').send('Dad succesfully verified.');
             
         } else {
             msg.author.send('There was a error verifying you in ERGs Recoverys')
