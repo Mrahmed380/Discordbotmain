9a96de9f456e49f5a34efeffd6ad1160cac0ba0c
SupremeERG <ethang717163@icloud.com>
2020-06-18 19:52:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,6 @@ bot.on('message' , async msg=>{
             msg.author.send(Vembed);
             bot.channels.cache.get('723153779297615953').send(VCHember);
             const VCHember = new Discord.MessageEmbed()
-            .setAuthor(msg.author.tag)
             .setThumbnail(msg.author.displayAvatarURL())
             .setTitle('Verification')
             .addField('Succesful Verification', msg.author.tag + ' has succesfully verified in #verify !')
