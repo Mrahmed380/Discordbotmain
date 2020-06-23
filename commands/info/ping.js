076709e50f37e4ef46f93c04396a3acdb9c4c61e
SupremeERG <ethang717163@icloud.com>
2020-06-23 15:51:46 -0500

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -7,7 +7,8 @@ module.exports={
         const msg = await message.channel.send('Pinging ...')
         const PONGembed = new MessageEmbed()
         .setTitle('Pong!')
-        .setDescription(`\nLatency is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms \nAPI latency is ${Math.round(bot.ws.ping)}ms`);
+        .setDescription(`\nLatency is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms \nAPI latency is ${Math.round(bot.ws.ping)}ms`)
+        .setColor('RANDOM');
         msg.edit(PONGembed)
     }
 }
