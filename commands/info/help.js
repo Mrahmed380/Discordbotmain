1a14623455730758781c5baf85fef805672f5f06
SupremeERG <ethang717163@icloud.com>
2020-06-23 16:04:20 -0500

    Create help.js

---
+++ commands/info/help.js
@@ -0,0 +1,15 @@
+const { Client , MessageEmbed } = require('discord.js');
+const { category } = require('../guild/unban');
+module.exports={
+    name: 'help',
+    category: 'info',
+    description: 'Shows you how to use a command and what it does!',
+    run: async(bot,message,args)=>{
+        if(bot.commands.has(command)) {
+            command = bot.commands.get(command)
+            const HELPembed = new MessageEmbed()
+            .setTitle('Command help')
+            .setDescription(`Prefix: e!\n**Command:** ${command.config.name}`)
+        }
+    }
+}
