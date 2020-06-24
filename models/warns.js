69a570bf0cb67d29f98d91f357b2c5c9f7040693
SupremeERG <ethang717163@icloud.com>
2020-06-24 17:30:51 -0500

    Update warns.js

--- models/warns.js
+++ models/warns.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 const Schema = new mongoose.Schema({
     Warns: Array,
     User: String,
-    Guild: String,ddd
+    Guild: String,
 })
 module.exports = mongoose.model("warns", Schema)
