ff52dfaec1ae630de7de860489621fca582080e8
SupremeERG <ethang717163@icloud.com>
2020-06-24 05:33:32 -0500

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -6,7 +6,7 @@ module.exports={
     usage: 'e!botinfo',
     run: async(bot,message,args)=>{
         var botinfo = 'Version 1.4.6, Created by ERG#1703 (bot is updated once a week)'
-        const INFOembed = new Discord.MessageEmbed()
+        const INFOembed = new MessageEmbed()
             .setTitle('All Commands + Bot info')
             .addField('commands' , 'menu, botinfo, prices, sells, moneydrop, stream, ping, cracked, serverjoin, botinv, methods, hgames, message @theUserYouWantToSendTo [The message you want to send to user], suggest, ticket, cticket [poll (Your message)], stats and [stats (@user)]. (prefix = e!)')
             .addField('Mod commands' , '[e!reject @user (Suggestion ID: 1234)], [e!approve @user (Suggestion ID: 1234)]')
