3e62e5ce9914d9cd9b9046283d37613cd5b67132
SupremeERG <ethang717163@icloud.com>
2020-06-24 03:40:52 -0500

    Update balance.js

--- commands/entertainment/balance.js
+++ commands/entertainment/balance.js
@@ -17,9 +17,9 @@ module.exports={
                 name: bot.users.cache.get(user.id).tag,
                 money: 0
             }
-            fs.writeFile('../entertainment/currency/money.json'), JSON.stringify(money), (err) => {
+            fs.writeFile('../entertainment/currency/money.json', JSON.stringify(money), (err) => {
                 if(err) console.log(err);
-            }
+            })
 
             return message.channel.send(`${bot.users.cache.get(user.id).username} has $${money[user.id].money}`);
 
