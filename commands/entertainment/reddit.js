3d0fcedbde1265f2727c0eba14c2657eb543da8d
SupremeERG <ethang717163@icloud.com>
2020-06-24 12:46:39 -0500

    Create reddit.js

---
+++ commands/entertainment/reddit.js
@@ -0,0 +1,23 @@
+const { Client , MessageEmbed } = require('discord.js');
+const api = require("imageapi.js")
+module.exports={
+    name: 'reddit',
+    description: 'Get a meme from a specific subreddit',
+    category: 'entertainment',
+    usage: 'e!reddit <subreddit>',
+    run: async(bot,message,args)=>{
+        let Subreddit =  message.content.slice(9)
+        if(!Subreddit)return message.channel.send('You did not specify the subreddit!')
+        try{
+            let img = await api(Subreddit)
+            const Embed = new MessageEmbed()
+            .setTitle(`Image from r/${Subreddit}`)
+            .setColor('RANDOM')
+            .setImage(img)
+            message.channel.send(Embed)
+        } catch(err){
+            return message.channel.send(err)
+        }
+        
+    }
+}
