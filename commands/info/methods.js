d4697019cae512036514957b5afe890f6b82beff
SupremeERG <ethang717163@icloud.com>
2020-06-24 15:00:22 -0500

    d

--- commands/info/methods.js
+++ commands/info/methods.js
@@ -2,14 +2,19 @@ const { Client , MessageEmbed } = require('discord.js');
 module.exports={
     name: 'methods',
     description: 'Payment methods for Recovery Service',
-    category: 'entertainment',
+    category: 'info',
     usage: 'e!methods',
     run: async(bot,message,args)=>{
+        let PayApp = [
+            "#18f051",
+            "#006eff"
+        ]
+        let ColerWheel = PayApp[Math.floor(Math.random()*(PayApp.length))]
         const Membed = new MessageEmbed()
         .setTitle('Payment Methods and Game')
         .setDescription(`PayPal - PayPal.Me/717163 https://PayPal.Me/717163 \nCashapp - $EthanGartica https://cash.app/$EthanGartica `)
         .addField('GTA Copy' , 'ERG only accepts Steam and Epic copies of Grand Theft Auto 5')
-        .setColor('RANDOM')
+        .setColor(ColerWheel)
         .setFooter('ERG#1703');
         message.channel.send(Membed)
     }
