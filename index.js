be5e53bf877131af9024382a57c1ea59c10289fd
SupremeERG <ethang717163@icloud.com>
2020-06-26 08:48:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ bot.on('message' , async message=>{
     if(command) command.run(bot,message,args)
     const mongoose = require('mongoose');
     mongoose.connect('mongodb+srv://SupremeERG:Ethang0508@supremeerg-tcd25.mongodb.net/Data?retryWrites=true&w=majority',{
-        useUnifiedTopology: true,
+        useUnifiedTopology: false,
         useNewUrlParser: true,
     })
     mongoose.connection.on('connected', () => {
