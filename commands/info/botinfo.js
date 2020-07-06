ea99544ef19e2e903013f979edb1ce07b2c83fc7
SupremeERG <ethang717163@icloud.com>
2020-07-06 04:27:40 -0500

    k

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -9,7 +9,7 @@ module.exports={
     run: async(bot,message,args)=>{
         const INFOembed = new MessageEmbed()
             .setTitle('All Commands + Bot info')
-            .addField('commands' , 'menu, botinfo, prices, sells, moneydrop, stream, ping, cracked, serverjoin, botinv, methods, hgames, rate, suggest, ticket, cticket, stats, penis, ban, clear, kick, unban, poll, reddit, meme and giveaway (prefix = e!)')
+            .addField('commands' , config.commands)
             .addField('Mod commands' , '[e!reject @user (Suggestion ID: 1234)], [e!approve @user (Suggestion ID: 1234)]')
             .setThumbnail(message.guild.iconURL())
             .addField('Information' , config.botinfo)
