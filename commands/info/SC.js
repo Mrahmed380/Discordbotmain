c56339e807bb0b1dbd802ea2cfda39276c634bcc
SupremeERG <ethang717163@icloud.com>
2020-07-05 19:23:04 -0500

    Update SC.js

--- commands/info/SC.js
+++ commands/info/SC.js
@@ -1,10 +1,10 @@
 const { Client , MessageEmbed } = require('discord.js');
 const config = require('../../config.json')
 module.exports={
-    name: 'SC',
+    name: 'sc',
     description: 'Shows ERG\'s Social Club Name',
     category: 'info',
-    usage: 'e!SC',
+    usage: 'e!sc',
     perms: 'Send Messages',
     run: async (bot,message,args)=>{
         message.channel.send(config.SC)
