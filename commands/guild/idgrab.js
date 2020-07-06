1ea3e839ce1b7e011bfc022faff048f8dbcb1644
SupremeERG <ethang717163@icloud.com>
2020-07-06 06:21:45 -0500

    Create idgrab.js

---
+++ commands/guild/idgrab.js
@@ -0,0 +1,15 @@
+const { Client , MessageEmbed  } = require('discord.js');
+const config = require('../../config.json')
+module.exports={
+    name: 'idgrab',
+    category: 'guild',
+    description: 'resolves a users id',
+    usage: 'e!idgrab <user>',
+    perms: 'Send Messages',
+    run: async (bot,message,args)=>{
+        const Mention = message.mentions.members.first();
+        if(!Mention) return message.channel.send('You did not mention the user you want to resolve *goofy*')
+        message.channel.send(`${Mention} ID is ${Mention.id}`)
+        
+    }
+}
