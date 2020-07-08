cdfa1690a0dbb4b8cad717e3bd709ef2ac440ccb
SupremeERG <ethang717163@icloud.com>
2020-07-07 22:41:01 -0500

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -11,7 +11,7 @@ module.exports={
             .setTitle('All Commands + Bot info')
             .addField('commands' , config.commands)
             .addField('Mod commands' , '[e!reject @user (Suggestion ID: 1234)], [e!approve @user (Suggestion ID: 1234)]')
-            .setThumbnail(message.guild.iconURL())
+            .setThumbnail(bot.user.displayAvatarURL())
             .addField('Information' , config.botinfo)
             .addField('Command Help', 'Use e!help to get help on any command!')
             .addField('Dedicated Server' , 'ERGs Recoverys')
