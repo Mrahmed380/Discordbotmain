ba6e758fc073e2056d55c71f986d02de8ac13422
SupremeERG <ethang717163@icloud.com>
2020-07-09 07:16:27 -0500

    Update rate.js

--- commands/entertainment/rate.js
+++ commands/entertainment/rate.js
@@ -11,7 +11,7 @@ module.exports={
             "Head",
             "Loyalness",
             "Riding",
-            "Gaming",
+            "Glizzy gobbling",
             "Cooking",
             "Twerking",
             "Clingyness",
