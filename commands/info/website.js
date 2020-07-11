a0f8a656daa3966a9ebdcbd5cfdaef441c275a5a
SupremeERG <ethang717163@icloud.com>
2020-07-11 05:48:44 -0500

    Create website.js

---
+++ commands/info/website.js
@@ -0,0 +1,10 @@
+module.exports={
+    name: 'website',
+    description: 'Sends a link to the recovery website',
+    category: 'info',
+    usage: 'e!website',
+    perms: 'Send Messages',
+    run: async (bot,message,args)=>{
+        message.reply('heres a link to the website! https://website-recovery.herokuapp.com')
+    }
+}
