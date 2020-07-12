9fd42409be13ba0ebc0d480e422b4113bd0caa3a
SupremeERG <ethang717163@icloud.com>
2020-07-12 12:20:19 -0500

    Update ticket.js

--- commands/guild/ticket.js
+++ commands/guild/ticket.js
@@ -6,9 +6,9 @@ module.exports={
     usage: 'e!ticket',
     perms: 'Send Messages',
     run: async(bot,message,args)=>{
-            const tickchan = message.guild.channels.cache.find(ch => ch.name == ctname)
             const tname = message.author.id;
             const ctname = "t-" + tname;
+            const tickchan = message.guild.channels.cache.find(ch => ch.name == ctname)
             if(message.guild.channels.cache.find(ch => ch.name == ctname)) return message.channel.send('You alredy have a ticket open, go to your ticket channel and type e!cticket to close your current ticket!')
             message.guild.channels.create(ctname , {
                 type: 'text' ,
