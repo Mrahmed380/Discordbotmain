176aa1c294172512cbda089250d6318d004275a2
SupremeERG <ethang717163@icloud.com>
2020-07-14 17:42:41 -0500

    Update ban.js

--- commands/guild/ban.js
+++ commands/guild/ban.js
@@ -9,7 +9,7 @@ module.exports={
     run: async(bot,message,args)=>{
         const banID = message.content.slice(6)
         const Mention = message.mentions.members.first()
-        const reason = args.slice(2)
+        const reason = args.slice(4)
         if(message.content.toLowerCase().startsWith('e!ban')) {
             if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You need administrator to use this command!')
             if(!Mention) return message.channel.send('You did not specify the user')
