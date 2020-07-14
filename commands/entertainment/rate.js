1e68bf4780b73cd39aa095cdbe470e068f8d0238
SupremeERG <ethang717163@icloud.com>
2020-07-14 15:08:58 -0500

    Update rate.js

--- commands/entertainment/rate.js
+++ commands/entertainment/rate.js
@@ -22,11 +22,13 @@ module.exports={
         let skillselect = skillrate[Math.floor(Math.random()*(skillrate.length))]
         const Mention = message.mentions.members.first();
         const egirlrate = message.content.slice(29)
+        const erole = message.guild.roles.cache.find(r => r.name === 'egirls')
+        const formattedMsgs = erole.members.map(m => `${m.tag}`)
         if(!Mention) return message.channel.send('You must mention the **egirl** you want to rate.')
          if(!egirlrate) return message.channel.send('You didnt give a rate')
          if(egirlrate > 10 ) return message.channel.send('Rates are out of 10, use a number 10 or below!')
          if(message.content.includes('/')) return message.channel.send('Rates are automatically out of 10! Choose a number 1-10')
-        if(!message.mentions.members.first().hasPermission('PRIORITY_SPEAKER')) return message.channel.send(`This user isnt a egirl stuped; ${config.egirl} `)
+        if(!message.mentions.members.first().hasPermission('PRIORITY_SPEAKER')) return message.channel.send(`This user isnt a egirl stuped; ${formattedMsgs}. `)
         message.delete()
         const embed = new MessageEmbed()
         .setTitle(`Egirl skill`)
