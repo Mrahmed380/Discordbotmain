dc92ed47596875ad8b6a43a82e8046f3e1c56d3b
SupremeERG <ethang717163@icloud.com>
2020-07-16 02:06:31 -0500

    c

--- models/money.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = new mongoose.Schema({
-    Money: Array,
-    UserID: String,
-    GuildID: String,
-})
-module.exports = mongoose.model("money", Schema)
