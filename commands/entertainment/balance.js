59c003abc9d666453cf88997bb94f9a50ca2d236
SupremeERG <ethang717163@icloud.com>
2020-07-16 01:47:42 -0500

    Update balance.js

--- commands/entertainment/balance.js
+++ commands/entertainment/balance.js
@@ -29,7 +29,7 @@ module.exports={
                 .setColor('RANDOM')
                 message.channel.send(Wembed)
             }else{
-                
+                data.deleteOne()
                 let newBalance = new moneys({
                     UserID: user.id,
                     GuildID: message.guild.id,
