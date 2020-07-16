a2e13e124be0bbc7d4c5d26a4e9740f6e577d5a5
SupremeERG <ethang717163@icloud.com>
2020-07-16 14:24:00 -0500

    Update warns.js

--- commands/guild/warns.js
+++ commands/guild/warns.js
@@ -9,7 +9,7 @@ module.exports={
     run: async(bot,message,args)=>{
         let user = message.mentions.members.first();
         if(!user) return message.channel.send('Specify a user!')
-        warns.findOne({ Guild: message.guild.id, User: user.id },async(err, data) => {
+        warns.find({ Guild: message.guild.id, User: user.id },async(err, data) => {
             if(err) console.log(`There was a error fix your game kid`)
             if(!data.length) return message.channel.send(`${user.user.tag} has not got any warns in this server`)
             let embed = new MessageEmbed()
