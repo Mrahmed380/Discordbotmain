bd743e5c6b8817d1ada4f63b55be93f3c20c8407
SupremeERG <ethang717163@icloud.com>
2020-07-17 09:50:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -227,42 +227,6 @@ bot.on('message', async msg => {
 
 
 
-    if (msg.author.bot) return;
-    if (msg.content.toLowerCase().startsWith('e!stats')) {
-        const args = msg.content.split(' ');
-        console.log(args);
-        if (args.length > 2) {
-            msg.channel.send('Incorrect Usage: e!stats | e!stats @User | e!stats self');
-        } else if (args.length === 2) {
-            const memberM = msg.mentions.members.first();
-            const STembed = new MessageEmbed()
-                .setAuthor(`${memberM.user.tag} (${memberM.id})`, memberM.user.displayAvatarURL())
-                .setThumbnail(memberM.user.displayAvatarURL())
-                .addField('Created on', memberM.user.createdAt.toLocaleString(), true)
-                .addField('Joined Server on', memberM.joinedAt, true)
-                .addField('Player is Kickable', memberM.kickable, false)
-                .addField('Player is Banable', memberM.bannable, false)
-                .addField('Prescence', memberM.presence.status, false)
-                .setDescription(`${memberM.roles.cache.map(role => role.toString()).join(' ')}`)
-            msg.channel.send(STembed);
-        } else {
-            const { guild } = msg;
-            const GSembed = new MessageEmbed()
-                .setAuthor(`${guild.name} (${guild.id})`, guild.iconURL())
-                .setThumbnail(guild.iconURL())
-                .addField('Created on', guild.createdAt.toLocaleString(), true)
-                .addField('Guild Owner', guild.owner.user.tag)
-                .addField('Total Members', guild.members.cache.filter(member => !member.user.bot).size)
-                .addField('Total Bots', guild.members.cache.filter(member => member.user.bot).size)
-                .addField('Total Channels', guild.channels.cache.size)
-                .setDescription(`${guild.roles.cache.map(role => role.toString()).join(' ')}`);
-            msg.channel.send(GSembed);
-
-
-        }
-
-    }
-
 
 
     const swearwords = ['nigga', 'nigger', 'Nigga', 'n i g g a', 'NIGGA', 'n i g g e r']
