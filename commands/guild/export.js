532a887a00c2a5e7cf4a4eefaf8395d024101a5b
SupremeERG <ethang717163@icloud.com>
2020-07-17 07:58:39 -0500

    Update export.js

--- commands/guild/export.js
+++ commands/guild/export.js
@@ -13,7 +13,6 @@ module.exports = {
     run: async (bot, message, args) => {
         let STL = args[0]
         if(!STL) return message.channel.send("You need to provide a server template link")
-        if(STL.startsWith !== "https://discord.new/") return message.channel.send('That is not a template link!')
         servers.findOne({ Guild: message.guild.id }, async (err, data) => {
             if (err) console.log(err)
             if (!data) {
