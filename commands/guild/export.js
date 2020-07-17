97538d250dbe137dba82f07766c41bb4ed65070e
SupremeERG <ethang717163@icloud.com>
2020-07-17 06:38:34 -0500

    Update export.js

--- commands/guild/export.js
+++ commands/guild/export.js
@@ -37,6 +37,7 @@ module.exports = {
                 }, 20000);
             } else {
                 message.channel.send("Data already exists")
+                console.log(data.ServerInfo[0])
                 /*data.Warns.unshift({
                     Moderator: message.author.id,
                     Reason: args.slice(1).join(" "),
