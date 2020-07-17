3a998491cf2b552896a022d3512f5a9dae66d5bd
SupremeERG <ethang717163@icloud.com>
2020-07-16 23:17:54 -0500

    Update kick.js

--- commands/guild/kick.js
+++ commands/guild/kick.js
@@ -8,10 +8,8 @@ module.exports = {
     run: async (bot, message, args) => {
         const Mention = message.mentions.members.first()
         if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You need administrator to use this commmand')
-        if (!message.content.includes('@')) return message.channel.send('You must mention someone to kick durdur goofy')
-        if (!Mention.kickable) {
-            return message.channel.send('You cannot kick this person')
-        }
+        if (!Mention) return message.channel.send('You must mention someone to kick durdur goofy')
+        if (!Mention.kickable) return message.channel.send('You cannot kick this person')
         Mention.send('You have been kicked from ' + (message.guild.name))
         Mention.kick();
         message.channel.send(`${Mention} was kicked from the server!`)
