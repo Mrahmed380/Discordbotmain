1ae9b987ea9788a973c541f417b4823b3e44f221
SupremeERG <ethang717163@icloud.com>
2020-07-16 20:28:18 -0500

    Update prefix.config.js

--- commands/info/prefix.config.js
+++ commands/info/prefix.config.js
@@ -10,7 +10,6 @@ module.exports={
     run: async(bot,message,args)=>{
         let newpre = args[0]
         if(!newpre) return message.channel.send("You need to specify the new prefix!")
-        message.channel.send(`New prefix \`${newpre}\``)
         prefix.findOne({ Guild: message.guild.id },async(err, data) => {
             if(err) console.log(err)
             if(!data){
@@ -18,18 +17,18 @@ module.exports={
                     Guild: message.guild.id,
                     Prefix: newpre
                 })
-                console.log(`New prefix ${newpre}`)
+                console.log(`New prefix \`${newpre}\``)
                 newPrefix.save()
-                message.channel.send(`prefix was changed from "e!" to ${newpre}`)
+                message.channel.send(`prefix was changed from \`"e!" to ${newpre}\``)
             }else{
                 data.Prefix = newpre
                 data.save()
                 console.log(data.Prefix)
                 const WWembed = new MessageEmbed()
-                .setTitle('Warning')
-                .setDescription(`new prefix`)
+                .setTitle('Prefix change')
+                .setDescription(`Prefix changed from \`${data.Prefix}\` to \`${newpre}\` `)
                 .setColor('RANDOM')
-                .setFooter(`new prefix ${data.Prefix}`)
+                .setFooter(`\`${data.Prefix}prefix\` <newPrefix> for new custom prefix`)
                 message.channel.send(WWembed)
                 }
             }
