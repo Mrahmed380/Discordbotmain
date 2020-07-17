244bfeaf13eea48352a1864bbdff448ec53dc5e7
SupremeERG <ethang717163@icloud.com>
2020-07-16 22:38:12 -0500

    Update ban.js

--- commands/guild/ban.js
+++ commands/guild/ban.js
@@ -1,34 +1,33 @@
-const { CLient , MessageEmbed } = require('discord.js');
+const { CLient, MessageEmbed } = require('discord.js');
 const { category } = require('./ban');
-module.exports={
+module.exports = {
     name: 'ban',
     category: 'guild',
     description: 'Bans any mentioned user',
     usage: 'e!ban <@user>',
     perms: 'Administrator',
-    run: async(bot,message,args)=>{
+    run: async (bot, message, args) => {
         const banID = message.content.slice(6)
         const Mention = message.mentions.members.first()
         const reason = args.slice(1).join(" ")
-        if(message.content.endsWith('ban')) {
-            if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You need administrator to use this command!')
-            if(!Mention) return message.channel.send('You did not specify the user')
-            if(!reason) return message.channel.send('You did not give a reason')
-            if(!Mention.bannable) {
-                message.channel.send('You cannot ban this user!!!')
-            } else {
-                const Bembed = new MessageEmbed()
-                .setTitle('Ban Hammer')
-                .setDescription(`${message.author} just banned yo ass!`)
-                .addField('Reason' , reason)
-                .setThumbnail(message.guild.iconURL())
-                .setColor(0xd5eb34)
-                .setFooter(message.guild.name);
-                Mention.send(Bembed);
-                Mention.ban({ reason: reason});
-                message.channel.send(`${Mention} was banned!`)
-            }
+        if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You need administrator to use this command!')
+        if (!Mention) return message.channel.send('You did not specify the user')
+        if (!reason) return message.channel.send('You did not give a reason')
+        if (!Mention.bannable) {
+            return message.channel.send('You cannot ban this user!!!')
         }
+        const Bembed = new MessageEmbed()
+            .setTitle('Ban Hammer')
+            .setDescription(`${message.author} just banned yo ass!`)
+            .addField('Reason', reason)
+            .setThumbnail(message.guild.iconURL())
+            .setColor(0xd5eb34)
+            .setFooter(message.guild.name);
+        Mention.send(Bembed);
+        Mention.ban({ reason: reason });
+        message.channel.send(`${Mention} was banned!`)
+
+
     }
 
 }
