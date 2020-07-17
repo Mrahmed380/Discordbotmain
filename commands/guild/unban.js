9beae3ebd51feeff545a623484564616ab83da6e
SupremeERG <ethang717163@icloud.com>
2020-07-16 23:04:11 -0500

    Update unban.js

--- commands/guild/unban.js
+++ commands/guild/unban.js
@@ -10,7 +10,7 @@ module.exports = {
         if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You dont have permission to unban people!')
         else {
             const ID = args[0]
-            if(!ID) return message.channel.send("***Give me the fucking id or else***<a:peeporain:593080847775825942>")
+            if(!ID) return message.channel.send("***Give me the fucking id or else***")
             message.guild.members.unban(ID)
             message.channel.send(`<@${ID}> was unbanned!`)
         }
