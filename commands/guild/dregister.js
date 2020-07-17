27e4abad9761802769e7e8813e3a07381159d2af
SupremeERG <ethang717163@icloud.com>
2020-07-16 23:45:22 -0500

    Update dregister.js

--- commands/guild/dregister.js
+++ commands/guild/dregister.js
@@ -8,7 +8,7 @@ module.exports={
     usage: 'dregister',
     perms: 'Send Messages',
     run: async(bot,message,args)=>{
-        gtas.findOne({ UserID: message.author.id, GuildID: message.guild.id },async(err, data) => {
+        gtas.findOne({ UserID: message.author.id },async(err, data) => {
             if(err) console.log(err)
             if(!data){
                 message.channel.send('You arent even in the database, register <SCName> to get logged')
