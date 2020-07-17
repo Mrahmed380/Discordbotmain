cba3cea580ce3478af286389cd7ae9e6c0b32d02
SupremeERG <ethang717163@icloud.com>
2020-07-17 08:04:42 -0500

    Update export.js

--- commands/guild/export.js
+++ commands/guild/export.js
@@ -12,7 +12,7 @@ module.exports = {
     perms: 'Send Messages',
     run: async (bot, message, args) => {
         let STL = args[0]
-        if(!STL) return message.channel.send("You need to provide a server template link")
+        if(!STL) return message.channel.send("You need to provide a server template link (Just write \"export none\" if you dont have access to template link)")
         servers.findOne({ Guild: message.guild.id }, async (err, data) => {
             if (err) console.log(err)
             if (!data) {
