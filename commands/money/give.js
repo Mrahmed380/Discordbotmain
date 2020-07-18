1f29ecc80c989d9e660828835a5779d17473b0cf
SupremeERG <ethang717163@icloud.com>
2020-07-18 08:42:20 -0500

    Create give.js

---
+++ commands/money/give.js
@@ -0,0 +1,48 @@
+const moneys = require('../../models/money')
+const { MessageEmbed } = require('discord.js')
+const { trimResultTransformer } = require('common-tags')
+module.exports = {
+    name: 'give',
+    description: 'Gives a user a number of coins',
+    usage: 'give <user> <amt of coins>',
+    category: 'money',
+    timeout: 10000,
+    perms: 'Admin',
+    run: async (bot, message, args) => {
+        const Mention = message.mentions.members.first()
+        let amt = args.slice(2).join(" ")
+        if(!Mention) return message.channel.send('You need to specify who you want to give to!')
+        if(!amt) return message.channel.send('You need to include the amount that you want to give!')
+        moneys.findOne({ User: Mention.id }, async (err, data) => {
+            if (err) console.log(err)
+            if (!data) {
+                let newMoney = new moneys({
+                    User: Mention.id,
+                    Money: [
+                        {
+                            Purchases: 0,
+                            Money: amt,
+                        }
+                    ]
+                })
+                newMoney.save()
+                console.log(`New money created Could not console log for dumb reason idk`)
+                const embed = new MessageEmbed()
+                    .setTitle('Coins')
+                    .setDescription(`You just gave $${amt} to ${Mention} he now has $${amt}`)
+                    .setColor('RANDOM')
+                message.channel.send(embed)
+            } else {
+                console.log(data.Money[0])
+                const WWembed = new MessageEmbed()
+                    .setTitle('Coins')
+                    .setDescription(`You just gave $${amt} to ${Mention}, the user now has $${data.Money[0].Money}`)
+                    .setColor('RANDOM')
+                    .setFooter(`Use coins command for your coins!`)
+                message.channel.send(WWembed)
+            }
+        }
+        )
+    }
+}
+
