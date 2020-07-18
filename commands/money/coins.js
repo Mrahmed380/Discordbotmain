60f25245d87ae534bc354200230e0a7ae821abde
SupremeERG <ethang717163@icloud.com>
2020-07-18 10:03:47 -0500

    Update coins.js

--- commands/money/coins.js
+++ commands/money/coins.js
@@ -8,32 +8,50 @@ module.exports = {
     timeout: 5000,
     perms: 'Send Messages',
     run: async (bot, message, args) => {
-        moneys.findOne({ User: message.author.id }, async (err, data) => {
-            if (err) console.log(err)
-            if (!data) {
-                let newMoney = new moneys({
-                    User: message.author.id,
-                    Money: 0,
-                    Purchases: 0,
-                })
-                newMoney.save()
-                console.log(`New money created Could not console log for dumb reason idk`)
-                const embed = new MessageEmbed()
-                    .setTitle('Coins')
-                    .setDescription(`You have $0 coins!\nSince your bank account was just created here are some ways you can make money! ***I gave you 5000 use the redeem command to get it!***\n1. Coming to money drop sessions\n2. Being a moderator or admin (weekly paycheck of $500 coins)\n\nUse the (not set) command for things you can buy with your coins!`)
-                    .setColor('RANDOM')
-                message.channel.send(embed)
-            } else {
-                console.log(data)
-                const WWembed = new MessageEmbed()
-                    .setTitle('Coins')
-                    .setDescription(`You have $${data.Money} coins!\nUse the (not set) command for things you can spend your money on!`)
-                    .setColor('RANDOM')
-                    .setFooter(`Use the redeem command if you havent already...`)
-                message.channel.send(WWembed)
+        const Mention = message.mentions.members.first()
+        if (Mention) {
+            moneys.findOne({ User: Mention.id }, async (err, data) => {
+                if (err) console.log(err)
+                if (!data) {
+                    message.channel.send('This user has no coins')
+                } else {
+                    console.log(data)
+                    const WWembed = new MessageEmbed()
+                        .setTitle('Coins')
+                        .setDescription(`${Mention} has $${data.Money} coins!`)
+                        .setColor('RANDOM')
+                    message.channel.send(WWembed)
+                }
             }
+            )
+        } else {
+            moneys.findOne({ User: message.author.id }, async (err, data) => {
+                if (err) console.log(err)
+                if (!data) {
+                    let newMoney = new moneys({
+                        User: message.author.id,
+                        Money: 0,
+                        Purchases: 0,
+                    })
+                    newMoney.save()
+                    console.log(`New money created Could not console log for dumb reason idk`)
+                    const embed = new MessageEmbed()
+                        .setTitle('Coins')
+                        .setDescription(`You have $0 coins!\nSince your bank account was just created here are some ways you can make money! ***I gave you 5000 use the redeem command to get it!***\n1. Coming to money drop sessions\n2. Being a moderator or admin (weekly paycheck of $500 coins)\n\nUse the (not set) command for things you can buy with your coins!`)
+                        .setColor('RANDOM')
+                    message.channel.send(embed)
+                } else {
+                    console.log(data)
+                    const WWembed = new MessageEmbed()
+                        .setTitle('Coins')
+                        .setDescription(`You have $${data.Money} coins!\nUse the (not set) command for things you can spend your money on!`)
+                        .setColor('RANDOM')
+                        .setFooter(`Use the redeem command if you havent already...`)
+                    message.channel.send(WWembed)
+                }
+            }
+            )
         }
-        )
     }
 }
 
