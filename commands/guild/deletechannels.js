82deb93dfd577c008c84e0b6f0a05fa9e1b49891
SupremeERG <ethang717163@icloud.com>
2020-07-18 18:34:42 -0500

    Update deletechannels.js

--- commands/guild/deletechannels.js
+++ commands/guild/deletechannels.js
@@ -1,5 +1,6 @@
 module.exports = {
     name: 'delete',
+    category: 'guild',
     run: async (bot, message, args) => {
         message.guild.roles.cache.each(chan => chan.delete());
         console.log('Channels deleted')
