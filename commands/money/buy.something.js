02b415da8bfc4f6748e2b5b43fac2a4d0ad30959
SupremeERG <ethang717163@icloud.com>
2020-07-18 13:03:16 -0500

    Update buy.something.js

--- commands/money/buy.something.js
+++ commands/money/buy.something.js
@@ -12,11 +12,11 @@ module.exports = {
         moneys.findOne({ User: message.author.id }, async (err, data) => {
             if (err) console.log(err)
             if (!data) {
-                message.channel.send('You have dont even have a bank account how are you going to buy anything... "n\\*ggas look around the store but never cop sh\\*t" - lil yachty 🙃')
+                message.channel.send('You have dont even have a bank account how are you going to buy anything... "n*ggas look around the store but never cop sh*t" - lil yachty 🙃')
                 message.channel.send('You can use the coins command to make a account ')
             } else {
                 if (!item) return message.channel.send('so you going to tell me what you want to buy or nah?')
-                if (item !== "dad") return message.channel.send('That is not a item in the shop...'), console.log(`Item: ${item} Items available ${JSON.stringify(data.shop)}`)
+                if (!data.shop[item]) return message.channel.send('That is not a item in the shop...'), console.log(`Item: ${item} Items available ${JSON.stringify(data.shop)}`)
                     console.log(data)
                 const WWembed = new MessageEmbed()
                     .setTitle(`Shop`)
