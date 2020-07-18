3cef725c7f2186d20fa3d8e7a6d05283a89f393a
SupremeERG <ethang717163@icloud.com>
2020-07-18 15:37:08 -0500

    Update buy.something.js

--- commands/money/buy.something.js
+++ commands/money/buy.something.js
@@ -25,7 +25,7 @@ module.exports = {
                 let nowpurchase = data.Purchases;
                 let newPurchaseAdd = parseInt(nowpurchase) + parseInt(1)
                 data.Purchases = newPurchaseAdd
-                data.inventory.insertOne(item, "test")
+                data.insertOne(item, "test")
                 data.save()
                 const WWembed = new MessageEmbed()
                     .setTitle(`Shop`)
