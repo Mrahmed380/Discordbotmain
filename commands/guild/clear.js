c38b23239c3184f02ec59f7d852ce7853e270bbd
SupremeERG <ethang717163@icloud.com>
2020-07-18 23:28:56 -0500

    Update clear.js

--- commands/guild/clear.js
+++ commands/guild/clear.js
@@ -17,6 +17,7 @@ module.exports={
             position: channel.rawPosition,
             rateLimitPerUser: channel.rateLimitPerUser,
         };
+        let newChannel = await message.guild.channels.create(channel.name, options);
         if(!message.member.hasPermission("ADMINISTRATOR")) {
             return message.reply("You need administrator to delete messages").then(m => m.delete(5000));
         }
@@ -30,8 +31,6 @@ module.exports={
         if(isNaN(args[0]) || parseInt(args[0]) <= 0 ) {
             return message.channel.send('This is not a number!').then(m => m.delete({ timeout: 5000}));
         }
-        let newChannel = await message.guild.channels.create(channel.name, options);
-        if(deleteAmount = "max") return message.channel.delete().then(newChannel.send('all messages deleted!'))
 
         message.channel.bulkDelete(deleteAmount, true)
         .catch(err => message.reply(`Something went wrong... ${err}`));
