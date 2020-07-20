21cee12d1105d6b606fd8225ec93f34dd7e8934f
SupremeERG <ethang717163@icloud.com>
2020-07-20 18:16:00 -0500

    Update 1.js

--- rules/1.js
+++ rules/1.js
@@ -1,6 +1,8 @@
 const { MessageEmbed, Message } = require('discord.js')
+module.exports=async(bot,message,args)=>{
 let embed = new MessageEmbed()
     .setTitle('Rule 1')
     .setDescription('1. No backstage Recruiting = Instant ban')
     .setFooter('DM mods and admins for rule suggestions')
 message.channel.send(embed)
+}
