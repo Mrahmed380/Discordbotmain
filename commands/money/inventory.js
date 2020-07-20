b713a6ccb852983a80a99df98aa56cf6cb4ba21f
SupremeERG <ethang717163@icloud.com>
2020-07-20 02:03:33 -0500

    Update inventory.js

--- commands/money/inventory.js
+++ commands/money/inventory.js
@@ -1,6 +1,7 @@
 const moneys = require('../../models/money')
 const { MessageEmbed } = require('discord.js')
 module.exports = {
+    status: false,
     name: 'inventory',
     description: 'Shows what items you have in your inventory',
     usage: 'inventory [user]',
