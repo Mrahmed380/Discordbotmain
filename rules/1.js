e01bb35ca73adddfaf8f8f4288bf8628c489c229
SupremeERG <ethang717163@icloud.com>
2020-07-20 17:50:33 -0500

    Update 1.js

--- rules/1.js
+++ rules/1.js
@@ -1,4 +1,8 @@
+const { run } = require("../commands/guild/rules.1-11");
+
 module.exports={
-    name: '1'
+    name: '1',
+    run: async(bot,message,args)=>{
+        message.channel.send(`1. No backstage recruiting, this = permanent ban`)
+    }
 }
-`1. No backstage recruiting, this = permanent ban`
