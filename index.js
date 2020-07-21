01b9171360cd7a270872721f7952c78ef1927d69
SupremeERG <ethang717163@icloud.com>
2020-07-21 17:10:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -115,37 +115,37 @@ bot.on('message', (message) => {
 
 bot.on('message', async message => {
     const prefix = require('./models/config')
-    if(message.channel.type == 'dm') {
+    if (message.channel.type == 'dm') {
         if (message.author.bot) return;
-            if (!message.content.startsWith(config.prefix)) return;
-            //if (!message.member) message.member = await message.guild.fetchMember(message);
-            const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-            const cmd = args.shift().toLowerCase();
-            if (cmd.length == 0) return;
-            const command = bot.commands.get(cmd)
-            if (command) {
-                if (command.status == false) {
-                    console.log('command is off')
-                    return message.channel.send("This command is currently under maintenance!")
-                }
-                if(command.dm == false && message.channel.type == 'dm') {
-                    console.log('command is guild only')
-                    return message.channel.send('This command is only available in a server!!')
-                }
-                if (command.timeout) {
-                    if (Timeout.has(`${message.author.id}${command.name}`)) {
-                        console.log(`User put in time out for ${command.name}`)
-                        return message.reply(`You can only use this command  every ${ms(command.timeout)}\n *every time you use the command before the timer ends it resets*!`)
-                    } else {
-                        console.log("put in time out")
-                        Timeout.add(`${message.author.id}${command.name}`)
-                        setTimeout(() => {
-                            Timeout.delete(`${message.author.id}${command.name}`)
-                        }, command.timeout);
-                    }
-                }
-                command.run(bot, message, args)
+        if (!message.content.startsWith(config.prefix)) return;
+        //if (!message.member) message.member = await message.guild.fetchMember(message);
+        const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+        const cmd = args.shift().toLowerCase();
+        if (cmd.length == 0) return;
+        const command = bot.commands.get(cmd)
+        if (command) {
+            if (command.status == false) {
+                console.log('command is off')
+                return message.channel.send("This command is currently under maintenance!")
             }
+            if (command.dm == false && message.channel.type == 'dm') {
+                console.log('command is guild only')
+                return message.channel.send('This command is only available in a server!!')
+            }
+            if (command.timeout) {
+                if (Timeout.has(`${message.author.id}${command.name}`)) {
+                    console.log(`User put in time out for ${command.name}`)
+                    return message.reply(`You can only use this command  every ${ms(command.timeout)}\n *every time you use the command before the timer ends it resets*!`)
+                } else {
+                    console.log("put in time out")
+                    Timeout.add(`${message.author.id}${command.name}`)
+                    setTimeout(() => {
+                        Timeout.delete(`${message.author.id}${command.name}`)
+                    }, command.timeout);
+                }
+            }
+            command.run(bot, message, args)
+        }
     }
     prefix.findOne({ Guild: message.guild.id }, async (err, data) => {
         if (!data) {
@@ -162,7 +162,7 @@ bot.on('message', async message => {
                     console.log('command is off')
                     return message.channel.send("This command is currently under maintenance!")
                 }
-                if(command.dm == false && message.channel.type == 'dm') {
+                if (command.dm == false && message.channel.type == 'dm') {
                     console.log('command is guild only')
                     return message.channel.send('This command is only available in a server!!')
                 }
@@ -196,7 +196,7 @@ bot.on('message', async message => {
                     console.log('command is off')
                     return message.channel.send("This command is currently under maintenance!")
                 }
-                if(command.dm == false && message.channel.type == 'dm') {
+                if (command.dm == false && message.channel.type == 'dm') {
                     console.log('command is guild only')
                     return message.channel.send('This command is only available in a server!!')
                 }
@@ -213,9 +213,11 @@ bot.on('message', async message => {
                         }, command.timeout);
                     }
                 }
-                if(message.content.startsWith(`?${command.alt}`)){
-                    console.log(`used command alt name ${command.alt}`)
-                    message.channel.send('test it is working papa')
+                if (command.alt) {
+                    if (message.content.startsWith(`?${command.alt}`)) {
+                        console.log(`used command alt name ${command.alt}`)
+                        message.channel.send('test it is working papa')
+                    }
                 }
                 command.run(bot, message, args)
             }
