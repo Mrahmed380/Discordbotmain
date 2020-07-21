c13937840f7312cb786ff89ae23975d0cb9e7a54
SupremeERG <ethang717163@icloud.com>
2020-07-21 01:26:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,38 @@ bot.on('message', (message) => {
 
 bot.on('message', async message => {
     const prefix = require('./models/config')
+    if(message.channel.type == 'dm') {
+        if (message.author.bot) return;
+            if (!message.content.startsWith(config.prefix)) return;
+            if (!message.member) message.member = await message.guild.fetchMember(message);
+            const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+            const cmd = args.shift().toLowerCase();
+            if (cmd.length == 0) return;
+            const command = bot.commands.get(cmd)
+            if (command) {
+                if (command.status == false) {
+                    console.log('command is off')
+                    return message.channel.send("This command is currently under maintenance!")
+                }
+                if(command.dm == false && message.channel.type == 'dm') {
+                    console.log('command is guild only')
+                    return message.channel.send('This command is only available in a server!!')
+                }
+                if (command.timeout) {
+                    if (Timeout.has(`${message.author.id}${command.name}`)) {
+                        console.log(`User put in time out for ${command.name}`)
+                        return message.reply(`You can only use this command  every ${ms(command.timeout)}\n *every time you use the command before the timer ends it resets*!`)
+                    } else {
+                        console.log("put in time out")
+                        Timeout.add(`${message.author.id}${command.name}`)
+                        setTimeout(() => {
+                            Timeout.delete(`${message.author.id}${command.name}`)
+                        }, command.timeout);
+                    }
+                }
+                command.run(bot, message, args)
+            }
+    }
     prefix.findOne({ Guild: message.guild.id }, async (err, data) => {
         if (!data) {
             if (err) console.log(err)
