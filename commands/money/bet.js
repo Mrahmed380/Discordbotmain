152e6f72ca7a79cf96f0abbcb28d1fe06089bfea
SupremeERG <ethang717163@icloud.com>
2020-07-22 18:47:54 -0500

    Update bet.js

--- commands/money/bet.js
+++ commands/money/bet.js
@@ -9,18 +9,20 @@ module.exports = {
     run: async (bot, message, args) => {
         let amt = args.slice(0).join(" ")
         if (!amt) return message.channel.send("You need to specify the amount of coins you want to gamble!")
-        if(isNaN(amt)) return message.channel.send('That is not a number *are you trying to gamble a sentence?*')
+        if (isNaN(amt)) return message.channel.send('That is not a number *are you trying to gamble a sentence?*')
         console.log(amt)
-        moneys.findOne({ User: message.author.id }), async (err, data) => {
+        moneys.findOne({ User: message.author.id }, async (err, data) => {
             if (err) console.log(err)
             if (!data) {
                 message.channel.send("You have no coins or a bank account use the coin command to make a coin account!")
                 console.log(`${message.author.username} has no coins to gamble`)
             } else {
                 console.log(data)
-                if(amt > data.Money) return message.channel.send(`You dont even have this many coins you can only gamble $${data.Money}`)
+                if (amt > data.Money) return message.channel.send(`You dont even have this many coins you can only gamble $${data.Money}`)
                 message.channel.send('***`test good job you just gambled :)`***')
             }
         }
+        )
     }
+
 }
