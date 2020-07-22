a41d3ddfc7dee524ca9eaf12ebd19fcb38706c3f
SupremeERG <ethang717163@icloud.com>
2020-07-21 23:17:53 -0500

    Update test.js

--- commands/entertainment/test.js
+++ commands/entertainment/test.js
@@ -1,13 +1,13 @@
 const married = require('../../models/marry')
-module.exports={
+module.exports = {
     divorce: 'divorce',
     description: 'divorce some random bum',
     usage: 'divorce',
     dm: false,
     timeout: 5000,
     perms: 'Send Messages',
-    run: async(bot,message,args)=>{
-        moneys.findOne({ User: message.author.id }, async (err, data) => {
+    run: async (bot, message, args) => {
+        married.findOne({ User: message.author.id }, async (err, data) => {
             if (err) console.log(err)
             if (!data) {
                 message.channel.send('You arent even married ¯\\_(ツ)_/¯')
