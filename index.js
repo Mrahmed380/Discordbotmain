191580d1276f334c7a0fc8e659f304a1c458d701
SupremeERG <ethang717163@icloud.com>
2020-07-21 20:58:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -134,16 +134,17 @@ bot.on('message', async message => {
                 return message.channel.send('This command is only available in a server!!')
             }
             if (command.timeout) {
-                let cooldown = used.get(`${message.author.id}${command.name}`)
+                let cooldown = used.get(message.author.id,command.name)
                 let remaining = Duration(cooldown - Date.now(), { units: ['h', 'm'], round: true})
                 if (cooldown) {
                     console.log(`User is in timeout ${command.name}`)
-                    message.reply(`You need to wait ${remaining}!`)
+                    return message.reply(`You need to wait ${remaining}!`)
                 } else {
                     console.log("put in time out")
-                    used.set(`${message.author.id}, ${Date.now() + 1000 * 60 * 60 * 24},${command.name}`)
+                    used.set(message.author.id, Date.now() + command.timeout,command.name)
                     setTimeout(() => {
                         used.delete((message.author.id),(command.name))
+                        console.log('deleted out of timeout')
                     }, command.timeout);
                 }
             }
@@ -170,16 +171,17 @@ bot.on('message', async message => {
                     return message.channel.send('This command is only available in a server!!')
                 }
                 if (command.timeout) {
-                    let cooldown = used.get(`${message.author.id}${command.name}`)
+                    let cooldown = used.get(message.author.id,command.name)
                     let remaining = Duration(cooldown - Date.now(), { units: ['h', 'm'], round: true})
                     if (cooldown) {
                         console.log(`User is in timeout ${command.name}`)
-                        message.reply(`You need to wait ${remaining}!`)
+                        return message.reply(`You need to wait ${remaining}!`)
                     } else {
                         console.log("put in time out")
-                        used.set(`${message.author.id}, ${Date.now() + 1000 * 60 * 60 * 24},${command.name}`)
+                        used.set(message.author.id, Date.now() + command.timeout,command.name)
                         setTimeout(() => {
                             used.delete((message.author.id),(command.name))
+                            console.log('deleted out of timeout')
                         }, command.timeout);
                     }
                 }
