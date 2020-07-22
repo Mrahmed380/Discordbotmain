53c11f7942d2b4d04d821dff220fe26500910cd0
SupremeERG <ethang717163@icloud.com>
2020-07-21 20:07:00 -0500

    Create daily.js

---
+++ commands/money/daily.js
@@ -0,0 +1,43 @@
+const ms = require('ms')
+const moneys = require('../../models/money')
+let cd = 8.64e+7;
+module.exports = {
+    name: 'daily',
+    description: 'Gives 1000 every 24 hrs',
+    usage: 'daily',
+    category: 'money',
+    dm: false,
+    timeout: cd,
+    perms: 'Send Messages',
+    run: async (bot, message, args) => {
+        moneys.findOne({ User: message.author.id }, async (err, data) => {
+            if (err) console.log(err)
+            if (!data) {
+                let newCoin = new moneys({
+                    Money: 1000,
+                    Purchases: 0,
+                    inventory: {
+                        CoinCard: 0
+                    },
+                    shop: {
+                        BasicRecovery: 75000,
+                        StandardRecovery: 100000,
+                        PremiumRecovery: 150000,
+                    },
+                })
+            } else {
+                console.log(data)
+                let oldMone = data.Money;
+                let newMon = parseInt(oldMone) + parseInt(1000)
+                data.Money = newMon;
+                data.save()
+                const WWembed = new MessageEmbed()
+                    .setTitle('Coins')
+                    .setDescription(`You just collected your daily reward!\nYou now have $${data.Money} coins!`)
+                    .setColor('RANDOM')
+                message.channel.send(WWembed)
+            }
+        }
+        )
+    }
+}
