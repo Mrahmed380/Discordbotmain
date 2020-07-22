926697dbe03dd9d90d100b54e57fcb9b3571160a
SupremeERG <ethang717163@icloud.com>
2020-07-21 23:03:09 -0500

    m

--- models/fake.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = new mongoose.Schema({
-    couple: String,
-    adopted: Array,
-    user: String,
-    user2: String,
-});
-
-module.exports = mongoose.model('marriage', Schema);
