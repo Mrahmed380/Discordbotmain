4f9f8aa3f242086f8c9601c218ffb9d9fcc5d858
SupremeERG <ethang717163@icloud.com>
2020-07-22 18:37:53 -0500

    Update bet.js

--- commands/money/bet.js
+++ commands/money/bet.js
@@ -7,14 +7,16 @@ module.exports = {
     dm: false,
     perms: "Send Messages",
     run: async (bot, message, args) => {
-        let amt = args[0]
+        let amt = args.slice(0).join(" ")
         if (!amt) return message.channel.send("You need to specify the amount of coins you want to gamble!")
+        console.log(amt)
         moneys.findOne({ User: message.author.id }), async (err, data) => {
             if (err) console.log(err)
             if (!data) {
                 message.channel.send("You have no coins or a bank account use the coin command to make a coin account!")
                 console.log(`${message.author.username} has no coins to gamble`)
             } else {
+                console.log(data)
                 if(amt > data.Money) return message.channel.send(`You dont even have this many coins you can only gamble $${data.Money}`)
                 message.channel.send('***`test good job you just gambled :)`***')
             }
