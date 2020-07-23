793d2e3d6bbb82740a7752f062927a0e7237dc2a
SupremeERG <ethang717163@icloud.com>
2020-07-23 18:09:24 -0500

    Update richest.js

--- commands/money/richest.js
+++ commands/money/richest.js
@@ -11,27 +11,27 @@ module.exports = {
     run: async (bot, message, args) => {
         const memIDs = message.guild.members.fetch(mem => mem.id);
         //fetch all the members id
-        money.find({ Guild: message.guild.id }).sort(['Money', 'descending']).exec((err, res) => {
-                if (err) console.log(err)
-                let rankEmbed = new Discord.MessageEmbed()
-                    .setTitle(`${message.guild.name} Richest Users`);
+        money.find({ Guild: message.guild.id, }).sort(['Money', 'descending']).exec((err, res) => {
+            if (err) console.log(err)
+            let rankEmbed = new Discord.MessageEmbed()
+                .setTitle(`${message.guild.name} Richest Users`);
 
-                let newRes = [];
-                res.forEach(elem => {
-                    if (elem.Money != 0) newRes.push(elem);
-                });
-                res = newRes;
+            let newRes = [];
+            res.forEach(elem => {
+                if (elem.Money != 0) newRes.push(elem);
+            });
+            res = newRes;
 
-                if (res.length == 0) {
-                    rankEmbed.addField('There are not any members in the database', '\u200b');
+            if (res.length == 0) {
+                rankEmbed.addField('There are not any members in the database', '\u200b');
+            }
+            else if (res.length > 10) {
+                for (let i = 0; i < 10; i++) {
+                    let elem = res[i];
+                    rankEmbed.addField(`${i + 1}. @${message.guild.members.cache.get(elem.User).user.tag}`, `${elem.Mone}`);
                 }
-                else if (res.length > 10) {
-                    for (let i = 0; i < 10; i++) {
-                        let elem = res[i];
-                        rankEmbed.addField(`${i + 1}. @${message.guild.members.cache.get(elem.User).user.tag}`, `${elem.Mone}`);
-                    }
-                }
-                message.channel.send(rankEmbed)
-            })
+            }
+            message.channel.send(rankEmbed)
+        })
     }
 }
