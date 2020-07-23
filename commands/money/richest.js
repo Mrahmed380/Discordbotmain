a46b94e72dc2bc175444992540297ccc3a5682ee
SupremeERG <ethang717163@icloud.com>
2020-07-23 17:57:03 -0500

    k

--- commands/money/richest.js
+++ commands/money/richest.js
@@ -11,11 +11,29 @@ module.exports = {
     run: async (bot, message, args) => {
         const memIDs = message.guild.members.fetch(mem => mem.id);
         //fetch all the members id
-        money.find({ Guild: message.guild.id }, async (err, data) =>{
-            const embed = new MessageEmbed()
-            .setTitle("Coins")
-            .setDescription('uhh no clue')
+        money.find({ Guild: message.guild.id }, async (err, data) => {
             message.channel.send(data.Money)
-        }).sort(data.Money, 'descending')
+        }).sort(
+            [Money, 'descending']).exec((err, res) => {
+                if (err) console.log(err)
+                let rankEmbed = new Discord.MessageEmbed()
+                    .setTitle(`${message.guild.name} Richest Users`);
+
+                let newRes = [];
+                res.forEach(elem => {
+                    if (elem.Money != 0) newRes.push(elem);
+                });
+                res = newRes;
+
+                if (res.length == 0) {
+                    rankEmbed.addField('There are not any members in the database', '\u200b');
+                }
+                else if (res.length > 10) {
+                    for (let i = 0; i < 10; i++) {
+                        let elem = res[i];
+                        rankEmbed.addField(`${i + 1}. @${message.guild.members.cache.get(elem.User).user.tag}`, `${elem.Mone}`);
+                    }
+                }
+            })
     }
 }
