40d9412b3f7327a70404313ca0363a378511c562
SupremeERG <ethang717163@icloud.com>
2020-07-24 01:24:28 -0500

    Update donate-to-homeless.js

--- commands/money/donate-to-homeless.js
+++ commands/money/donate-to-homeless.js
@@ -12,7 +12,7 @@ module.exports = {
         let amt = args.slice(0).join(" ")
         if (!amt) return message.channel.send("You need to specify the amount of coins you want to give!")
         if (isNaN(amt)) return message.channel.send('??!!')
-        if(amt < 1) return message.channel.send('why would you give a homeless person $0 thats mean')
+        if (amt < 1) return message.channel.send('why would you give a homeless person $0 thats mean')
         console.log(amt)
         moneys.findOne({ User: message.author.id }, async (err, data) => {
             if (err) console.log(err)
@@ -20,22 +20,23 @@ module.exports = {
                 message.channel.send("You have no coins or a bank account use the coin command to make a coin account!")
                 console.log(`${message.author.username} has no coins to gamble`)
             } else {
-                if(data.Money == 0) return message.channel.send(`You dont even have coins`)
+                if (data.Money == 0) return message.channel.send(`You dont even have coins`)
                 if (amt > data.Money) return message.channel.send(`You dont even have this many coins you can only give $${data.Money}`)
                 let chances = ["win", "lose", ";ajf", "other", "ok"];
                 const pick = chances[Math.floor(Math.random() * (chances.length))];
                 let newMoney = Math.round(Math.random() * amt * 5);
-                if(pick == "win") {
-                    data.Money += newMoney
+                let RoundedCoins = Math.ceil(newMoney / 10 * 10);
+                if (pick == "win") {
+                    data.Money += RoundedCoins
                     data.save();
                     console.log(`${pick} - ${newMoney}`)
-                    message.channel.send(`It turns out the person you just gave money was a random youtuber doing a social experiment and gave you $${newMoney}!`)
+                    message.channel.send(`It turns out the person you just gave money was a random youtuber doing a social experiment and gave you $${RoundedCoins}!`)
                 } else {
                     data.Money -= amt;
                     data.save();
                     console.log(`${pick} - ${newMoney}`)
                     message.channel.send(`${personPick} was thankful for the money!`)
-                } 
+                }
                 console.log(data)
             }
         }
