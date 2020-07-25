0fb231b0e01faf2a8f73f9de4eabfd6f7f72184a
SupremeERG <ethang717163@icloud.com>
2020-07-25 03:35:19 -0500

    Update fight-tryone.js

--- commands/money/fight-tryone.js
+++ commands/money/fight-tryone.js
@@ -1,36 +1,45 @@
 const money = require('../../models/money');
 module.exports = {
     name: 'fight',
-    description: 'Fight a random person  ',
+    description: 'Fight a random person and fold them for there money ',
     category: 'money',
     timeout: 15000,
     dm: true,
-    usage: 'fight',
+    usage: 'fight [user]',
     perms: 'Send Messages',
     aliases: ['fold'],
     run: async (bot, message, args) => {
+        let mention = message.mentions.members.first();
+        if(!mention) return message.channel.send('You need to mention you want to fight!');
         money.findOne({ User: message.author.id }, async (err, data) => {
-            if (err) console.log(err);
-            if (!data || data.Money == 0) return message.channel.send("You have no money haha💀");
-            let amt = Math.round(Math.random() * 3000);
-            let lose$ = Math.floor(data.Money / 4 * 3)
-            let chances = ["win", "dad", "pad", "lose"]
-            const pick = chances[Math.floor(Math.random() * (chances.length))];
-            let fighters = ["Hakeem", "Tyrone", "JJ", "Djay", "Aaron", "Jamal", "Zion"]
-            const fightpick = fighters[Math.floor(Math.random() * (fighters.length))];
-            if (pick == "lose") {
-                message.channel.send(`You tried to run ${fightpick}'s pocket's but realize he got that ***blicky icky*** on him and take 2 shots to the chest and as you \`crawl\` away he takes your wallet with $${lose$} 🤡`);
-                console.log(`user lost ${data.Money} - ${lose$}`);
-                data.Money -= lose$;
-                data.save();
-                console.log(data);
-            } else {
-                message.channel.send(`You randomly decide to hit ${fightpick} with that mink mink and have him folded and as he folds like oragami you quickly run his pockets grabbing **$${amt}**\n***\`Black Air Force Activity\`***`)
-                console.log(`user win ${data.Money} + ${amt}`)
-                data.Money += amt;
-                data.save();
-                console.log(data.Money)
-            }
+            money.findOne({ User: mention.id }, async (erro, dat) => {
+                if (erro) console.log(err)
+                if (err) console.log(err);
+                if (!data || data.Money == 0) return message.channel.send("you have no money💀");
+                if(data.passive == true) return message.channel.send('You are in passive! You can not use commands that can harm other users.')
+                if(!dat || dat.Money == 0 || dat.passive == true) return message.channel.send('This user is either in passive mode or has no money!');
+                let amt = Math.round(Math.random() * dat.Money);
+                let lose$ = Math.floor(data.Money / 4 * 3)
+                let chances = ["win", "dad", "pad", "lose"]
+                const pick = chances[Math.floor(Math.random() * (chances.length))];
+                if (pick == "lose") {
+                    message.channel.send(`You tried to run ${mention.user.username}'s pocket's but realize he got that ***blicky icky*** on him and take 2 shots to the chest and as you \`crawl\` away he takes your wallet with $${lose$} 🤡`);
+                    console.log(`user lost ${data.Money} - ${lose$}`);
+                    data.Money -= lose$;
+                    dat.Money += lose$;
+                    data.save();
+                    console.log(data);
+                } else {
+                    message.channel.send(`You randomly decide to hit ${mention.user.username} with that mink mink and have him *or her* folded and as he folds like oragami you quickly run his pockets grabbing **$${amt}**\n***\`Black Air Force Activity\`***`)
+                    console.log(`user win ${data.Money} + ${amt}; user who lost ${dat.Money} - ${amt}`);
+                    data.Money += amt;
+                    dat.Money -= amt;
+                    data.save();
+                    dat.save()
+                    console.log(data.Money);
+                    console.log(dat.Money);
+                }
+            })
         })
     }
 }
