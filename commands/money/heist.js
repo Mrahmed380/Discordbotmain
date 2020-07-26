317f2d1cfa80364e87663c1fd34a7703b710b4c1
SupremeERG <ethang717163@icloud.com>
2020-07-26 05:49:08 -0500

    Update heist.js

--- commands/money/heist.js
+++ commands/money/heist.js
@@ -1,7 +1,7 @@
 const money = require('../../models/money');
 module.exports = {
     name: 'heist',
-    status: true,
+    status: false,
     aliases: ['bankrob'],
     usage: 'heist',
     description: 'Rob a bank and get up to $100000',
@@ -11,15 +11,23 @@ module.exports = {
     run: async (bot, message, args) => {
         money.findOne({ User: message.author.id }, async (err, data) => {
             if (err) console.log(err)
-            if (!data || data.money < 1000) return message.channel.send("You need to pay $10000 to start a heist you dont have this money yet!")
+            if (!data || data.Money < 1000) return message.channel.send("You need to pay $10000 to start a heist you dont have this money yet!")
             message.channel.send(`heist from ${message.author} react to enter`).then(msg => {
                 msg.react("718678524101132288")
                 msg.reactions.cache.get("718678524101132288")
                 setTimeout(() => {
                     //if (msg.reactions.cache.size < 3) return message.channel.send('less than 2 people reacted so the heist was canceled you must wait another hour until you start another heist!')
                     let winner = msg.reactions.cache.get("718678524101132288").users.cache.filter(u => !u.bot)
-                    let wins = winner.map(user => `${user}, `);
-                    message.channel.send(wins)
+                    let wins = winner.map(user => `${user.user.tag}, `);
+                    message.channel.send(`Users in the heist ${wins} wait 30 seconds while I calculate the results...`).then(r => r.delete({ timeout: 30000 }))
+                    setTimeout(() => {
+                        console.log(wins);
+                        let amt = Math.random(10000)
+                        
+                        let DivAmt = Math.round(amt / msg.reactions.cache.size);
+                        data.Money += DivAmt;
+                        data.save()
+                    }, 30000);
                 }, 30000);
             })
         })
