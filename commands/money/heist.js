b3cb5ed5b262261b5ad4d00d08a15987c49f5e61
SupremeERG <ethang717163@icloud.com>
2020-07-26 04:58:06 -0500

    Create heist.js

---
+++ commands/money/heist.js
@@ -0,0 +1,20 @@
+const money = require('../../models/money');
+module.exports={
+    name: 'heist',
+    status: true,
+    aliases: ['bankrob'],
+    usage: 'heist',
+    description: 'Rob a bank and get up to $100000',
+    timeout: 3.6e+6,
+    category: 'money',
+    perms: "Send Messages",
+    run: async(bot,message,args)=>{
+        money.findOne({ User: message.author.id }, async (err,data)=>{
+            if(err) console.log(err)
+            if(!data || data.money == 0) return message.channel.send("You need to pay $10000 to start a heist you dont have this money yet!")
+            message.channel.send(`heist from ${message.author} react to enter`).then(msg => {
+                msg.react("<:RecoveryIsEmo:718678524101132288>")
+            })
+        })
+    }
+}
