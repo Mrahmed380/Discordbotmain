f46c881b80051d2efb491c1451cd118f4171d5b2
SupremeERG <ethang717163@icloud.com>
2020-07-26 21:42:20 -0500

    Create announce.js

---
+++ commands/guild/announce.js
@@ -0,0 +1,17 @@
+module.exports={
+    name: 'say',
+    usage: 'say <channel> <message>',
+    description: 'Announce a specific mesage',
+    aliases: ['announce', 'yell'],
+    timeout: 30000,
+    perms: 'Admin',
+    run: async(bot,message,args)=>{
+        const admin = message.guild.roles.find(r => r.name === 'Admin');
+        if(!message.member.roles.cache.has(admin.id)) return message.channel.send('You need admin role for this command!');
+        let chan = message.mentions.channels.first();
+        if(!chan) return message.channel.send('You need to specify the channel!');
+        let msg = args.slice(1).join(" ");
+        if(!msg) return message.chan.send('You need to give a message!');
+        chan.send(`Announcement from ${message.author.tag}\n${msg} @everyone`);
+    }
+}
