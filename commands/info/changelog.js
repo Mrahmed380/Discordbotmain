95f9e31f7498400ecfa33c7492dd8aa12cad2ee7
SupremeERG <ethang717163@icloud.com>
2020-07-27 05:56:38 -0500

    s

---
+++ commands/info/changelog.js
@@ -0,0 +1,33 @@
+module.exports = {
+    name: "changelog",
+    aliases: ['changes', 'log', 'updates', 'update'],
+    description: 'Get a log of last 5 bot updates',
+    usage: 'changelog',
+    category: 'info',
+    dm: true,
+    perms: 'Send Messages',
+    run: async (bot, message, args) => {
+        const Discord = require('discord.js');
+        const GitHub = require('octonode');
+
+        const Client = GitHub.client(process.env.GITHUB_API_TOKEN);
+
+        let repo = Client.repo('SupremeERG/discordbotmain');
+
+        repo.commits((err, res) => {
+            if (err) console.error(err);
+
+            let info = res;
+
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(info[0].committer.login, info[0].committer.avatar_url, info[0].committer.html_url)
+                .setTitle(`Last 5 Changes for ${client.user.username}`);
+            for (let i = 0; i < 5; i++) {
+                embed.addField(`\u200b`, `${info[i].commit.message}`);
+            }
+
+            message.channel.send({ embed });
+
+        });
+    }
+}
