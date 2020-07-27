39364e3517572553af06f16b70f92a52b474a0f2
SupremeERG <ethang717163@icloud.com>
2020-07-26 21:49:00 -0500

    Update announce.js

--- commands/guild/announce.js
+++ commands/guild/announce.js
@@ -10,7 +10,7 @@ module.exports={
         let chan = message.mentions.channels.first();
         if(!chan) return message.channel.send('You need to specify the channel!');
         let msg = args.slice(1).join(" ");
-        if(!msg) return message.chan.send('You need to give a message!');
+        if(!msg) return message.channel.send('You need to give a message!');
         chan.send(`Announcement from ${message.author.tag}\n${msg} @everyone`);
     }
 }
