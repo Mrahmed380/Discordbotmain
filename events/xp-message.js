6c13c4a925c4b9b30d43d825776451472236416d
SupremeERG <ethang717163@icloud.com>
2020-07-28 00:27:48 -0500

    Update xp-message.js

--- events/xp-message.js
+++ events/xp-message.js
@@ -14,7 +14,7 @@ module.exports = async (message) => {
         } else {
             let xpNeeded = data.level * 500 + 90 - 17;
             let oldxp = data.xp;
-            let calculus = data.level * 2 + 31 + 89 * 2;
+            let calculus = parseInt(xpNeeded) / parseInt(data.level)
             let newxp = parseInt(oldxp) + parseInt(calculus);
             data.xp = newxp;
             if (data.xp >= xpNeeded) {
