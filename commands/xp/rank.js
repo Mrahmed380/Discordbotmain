4f9dd89da685afe3b8bd21d896a79d7f7619cc3a
SupremeERG <ethang717163@icloud.com>
2020-07-28 01:56:03 -0500

    Update rank.js

--- commands/xp/rank.js
+++ commands/xp/rank.js
@@ -1,28 +1,30 @@
 const xp = require('../../models/xp');
 const { model } = require('mongoose');
 const { MessageEmbed } = require('discord.js');
-module.exports={
+module.exports = {
     name: 'rank',
     description: 'shows xp',
     status: true,
     dm: true,
     usage: "rank",
     category: 'xp',
-    run: async(bot,message,args)=>{
+    run: async (bot, message, args) => {
         xp.findOne({ User: message.author.id }, async (err, data) => {
             //let xpNeeded = data.level * 50 + 500
-            if(err) console.log(err);
-            if(!data) {
+            if (err) console.log(err);
+            if (!data) {
                 console.log('data created');
                 message.channel.send('You are level 0 and have no xp')
             } else {
                 let xpNeeded = data.level * 500 + 90 - 17;
                 //let newXp = parseInt()
-                const xpembed = new MessageEmbed()
-                .setTitle(`${message.author.tag}'s Level and XP`)
-                .setDescription(`XP - ${data.xp}\nLevel - ${data.level}\nXP needed until next level - ${parseInt(xpNeeded) - parseInt(data.xp)} `)
-                .setColor(`PURPLE`)
-                message.channel.send(xpembed);
+                setTimeout(() => {
+                    const xpembed = new MessageEmbed()
+                        .setTitle(`${message.author.tag}'s Level and XP`)
+                        .setDescription(`XP - ${data.xp}\nLevel - ${data.level}\nXP needed until next level - ${parseInt(xpNeeded) - parseInt(data.xp)} `)
+                        .setColor(`PURPLE`)
+                    message.channel.send(xpembed);
+                }, 500);
             }
         })
     }
