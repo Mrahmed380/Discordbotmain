c69b9287186d27f1ac32e308025829207681a6d3
SupremeERG <ethang717163@icloud.com>
2020-07-27 23:44:47 -0500

    Update xp-message.js

--- events/xp-message.js
+++ events/xp-message.js
@@ -1,7 +1,6 @@
 module.exports=async(message)=>{
     const xp = require('../models/xp');
     xp.findOne({ User: message.author.id }, async (err, data) => {
-        let xpNeeded = data.level * 50 + 500
         if(err) console.log(err);
         if(!data) {
             let newLevel = new xp({
@@ -10,8 +9,9 @@ module.exports=async(message)=>{
                 level: 1,
             })
             newLevel.save();
-            message.channel.send('You are level 0 and have no xp')
+            //message.channel.send('You are level 0 and have no xp')
         } else {
+            let xpNeeded = data.level * 50 + 500
             let oldxp = data.xp;
             let newxp = parseInt(oldxp) + parseInt(100);
             data.xp = newxp;
