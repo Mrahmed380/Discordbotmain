98064af2c88ee55d018294a82392722aca91f052
SupremeERG <ethang717163@icloud.com>
2020-07-29 03:34:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -422,20 +422,28 @@ bot.on('guildMemberAdd', async (member) => {
 
     // To compare, we need to load the current invite list.
     member.guild.fetchInvites().then(guildInvites => {
-        // This is the *existing* invites for the guild.
-        const ei = invites[member.guild.id];
-        // Update the cached invites for the guild.
-        invites[member.guild.id] = guildInvites;
-        // Look through the invites, find the one for which the uses went up.
-        const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-        // This is just to simplify the message being sent below (inviter doesn't have a tag property)
-        const inviter = bot.users.get(invite.inviter.id);
-        // Get the log channel (change to your liking)
-        const logChannel = member.guild.channels.find(channel => channel.name === "『🤘🏻』new-clients");
-        // A real basic message with the information we need. 
-        logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+        const invites = {};
+
+
+        // Load all invites for all guilds and save them to the cache.
+        bot.guilds.cache.forEach(g => {
+            g.fetchInvites().then(guildInvites => {
+                invites[g.id] = guildInvites;
+            });
+        });
     });
+    const ei = invites[member.guild.id];
+    // Update the cached invites for the guild.
+    invites[member.guild.id] = guildInvites;
+    // Look through the invites, find the one for which the uses went up.
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    // This is just to simplify the message being sent below (inviter doesn't have a tag property)
+    const inviter = bot.users.get(invite.inviter.id);
+    // Get the log channel (change to your liking)
+    const logChannel = member.guild.channels.find(channel => channel.name === "『🤘🏻』new-clients");
+    logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+});
 
 
-})
+
 bot.login(process.env.token);
