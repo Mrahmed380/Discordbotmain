8394eddaea11e4b36568f31881bc368e71926fab
SupremeERG <ethang717163@icloud.com>
2020-07-28 23:58:25 -0500

    Added invite tracker for new members

---
+++ events/invites.js
@@ -0,0 +1,40 @@
+module.exports = async (bot) => {
+    let invites = {}
+    const getInviteCounts = async (guild) => {
+        return await new Promise(resolve => {
+            guild.fetchInvites().then(invites => {
+                const inviteCounter = {}
+
+                invites.forEach(invite => {
+                    console.log(`INVITE: ${invite}`);
+                    const { uses, inviter } = invite
+                    const { username, discriminator } = inviter
+                    const name = `${username}#${discriminator}`
+
+                    inviteCounter[name] = (inviteCounter[name] || 0) + uses;
+                })
+            })
+        })
+    }
+    bot.guilds.cache.forEach(guild => {
+        invites[guild.id] = await getInviteCounts(guild)
+        console.log(`Invites: ${invites[guild.id]}`);
+    })
+
+    bot.on('guildMemberADd', member => {
+        let chan = guild.channels.cache.find(c => c.name === '『🤘🏻』invites')
+        const { guild } = member;
+        const invitesBefore = invites[guild.id]
+        const invitesAfter = await getInviteCounts(guild);
+        console.log(`BEFORE ${invitesBefore}`);
+        console.log(`AFTER ${invitesAfter}`);
+        for (const inviter in invitesAfter) {
+            if (invitesBefore[inviter] === invitesAfter[inviter] - 1) {
+                const count = invitesAfter[inviter]
+                chan.send(`${member} joined!, invited by ${inviter} (${count} total invites)`)
+                invites[guild.id] = invitesAfter;
+                return
+            }
+        }
+    })
+}
