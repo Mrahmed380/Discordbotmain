dd10947287a4c289db223eb741e8ffb7717db9b2
SupremeERG <ethang717163@icloud.com>
2020-07-28 21:09:54 -0500

    Create invites.js

---
+++ commands/guild/invites.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: 'invites',
+    aliases: ['invite'],
+    description: 'Fetch the guild invites',
+    category: 'guild',
+    perms: 'Send Messages',
+    dm: false,
+    usage: 'invites',
+    run: async (bot, message, args) => {
+        const { guild } = message;
+
+        guild.fetchInvites().then((invites) => {
+            const inviteCounter = {}
+
+            invites.forEach((invite) => {
+                console.log(`INVITE: ${invite}`)
+                const { uses, inviter } = invite;
+                const { username, discriminator } = inviter;
+                const name = `${username}#${discriminator}`;
+                inviteCounter[name] = (inviteCounter[name]) + uses;
+            })
+            let replyText = 'Invites:'
+
+            for(const invite in inviteCounter){
+                const count = inviteCounter[invite]
+                replyText += `\n${invite} has invited ${count} members`
+            }
+            const embed = new MessageEmbed()
+                .setTitle('Invites')
+                .setDescription(`${replyText}`)
+                .setColor('RANDOM')
+                .setFooter('Invites for ' + message.guild.name)
+            message.channel.send(embed)
+        })
+    }
+}
