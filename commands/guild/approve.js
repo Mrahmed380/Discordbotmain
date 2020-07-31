5f830f5d6f38975b1b085c6f66bbe2bc4f6cbe19
SupremeERG <ethang717163@icloud.com>
2020-07-31 04:25:58 -0500

    Update approve.js

--- commands/guild/approve.js
+++ commands/guild/approve.js
@@ -6,7 +6,10 @@ module.exports = {
     category: 'guild',
     perms: "Moderator role or Admin role",
     run: async (bot, message, args) => {
-        const id = args.slice(1).join(" ");
+        const mod = message.guild.roles.cache.find(r => r.name === "Moderator");
+        const admin = message.guild.roles.cache.find(r => r.name === "Admin");
+        if (!message.member.roles.cache.has(mod || admin)) return message.channel.send("you cannot use this command!")
+            const id = args.slice(1).join(" ");
         const mention = message.mentions.members.first();
         if (!mention) return message.channel.send("You need to mention the user you want to approve of");
         if (!id) return message.channel.send('You need to include the id of the suggestion!');
