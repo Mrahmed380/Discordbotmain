e51433a5c8df1beb60bd1ad53cf4f7b0bf387b78
SupremeERG <ethang717163@icloud.com>
2020-07-31 16:18:58 -0500

    Update heist.js

--- commands/money/heist.js
+++ commands/money/heist.js
@@ -14,8 +14,6 @@ module.exports = {
             if (!data || data.Money < 10000) return message.channel.send("You need to pay $10000 to start a heist you dont have this money yet!")
             message.channel.send(`heist from ${message.author} react to enter\n**YOU HAVE 30 SECONDS**`).then(msg => {
                 msg.react("718678524101132288")
-                const filt = msg.reactions.cache.get("718678524101132288").users.cache.filter(u => !u.bot);
-                msg.reactions.cache.get("718678524101132288");
                 setTimeout(() => {
                     let reactors = new Map();
                     //if (msg.reactions.cache.size < 3) return message.channel.send('less than 2 people reacted so the heist was canceled you must wait another hour until you start another heist!')
@@ -25,6 +23,7 @@ module.exports = {
                     let wins = winner.map(user => `${user.tag}, `);
                     message.channel.send(`Users in the heist ${wins}\n wait 30 seconds while I calculate the results...`).then(r => r.delete({ timeout: 30000 }))
                     setTimeout(() => {
+                        const filt = await msg.reactions.cache.get("718678524101132288").users.cache.filter(u => !u.bot);
                         console.log(wins);
                         const get = reactors.get(`${winner}`);
                         console.log(winner)
