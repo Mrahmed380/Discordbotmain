777703c6f24ff6d807b8613ff67da4de83a11c7b
SupremeERG <ethang717163@icloud.com>
2020-07-31 04:20:53 -0500

    Update suggest.js

--- commands/guild/suggest.js
+++ commands/guild/suggest.js
@@ -25,7 +25,7 @@ module.exports = {
             .addField('Suggestion', mssArgs)
             .addField('suggestion ID', sgstID)
             .setFooter('Please wait while a moderator or admin rejects or accepts your suggestion. (note check your DMs.)');
-        bot.idMap.set(`${message.author.id}${sgstID}`)
+        bot.idMap.set(`${message.author.id}${sgstID}`, "suggestion")
         console.log(bot.idMap)
         message.channel.send(sembed).then(messageReaction => {
             messageReaction.react("✅")
