5d73f6d95b35126a0b78e036090fdb7ce3d7f28c
SupremeERG <ethang717163@icloud.com>
2020-08-01 05:18:03 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 module.exports = async (member) => {
     const channel = member.guild.channels.cache.find(channel => channel.name === "『🤘🏻』new-clients");
     if (!channel) return;
-    const Wembed = new Discord.MessageEmbed()
+    const Wembed = new MessageEmbed()
         .setTitle('Welcome')
         .addField('ERGs Recoverys', `Welcome to ERGs Recoverys, ${member}`)
         .addField('Verification', 'Please Verify your self to get accest to the rest of the channel.')
