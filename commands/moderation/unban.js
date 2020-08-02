ba89559dab94a75fc1336777bd95c0c895f86af9
SupremeERG <ethang717163@icloud.com>
2020-08-02 04:47:25 -0500

    Fixed unban error

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -7,8 +7,8 @@ module.exports = {
     usage: 'unban <usersID>',
     perms: 'Send Messages',
     run: async (bot, message, args) => {
-        const ID = message.content.slice(7);
-        if (!message.member.roles.cache.has('713914961285349397')) return message.channel.send('You dont have permission to unban people!')
+        const mod = message.guild.roles.cache.find(r => r.name === "Moderator")
+        if (!message.member.roles.cache.has(mod.id)) return message.channel.send('You dont have permission to unban people!')
         else {
             const ID = args[0]
             if(!ID) return message.channel.send("***Give me the fucking id or else***")
