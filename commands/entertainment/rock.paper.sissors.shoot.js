b5237e5cfe429664e8d9d34b0f416faa50bd4034
SupremeERG <ethang717163@icloud.com>
2020-08-01 22:59:49 -0500

    Update rock.paper.sissors.shoot.js

--- commands/entertainment/rock.paper.sissors.shoot.js
+++ commands/entertainment/rock.paper.sissors.shoot.js
@@ -7,8 +7,10 @@ module.exports = {
     status: true,
     perms: "Send Messages",
     run: async (bot, message, args) => {
-            const results = ["rock", "paper", "scissors"]
-            const results2 = ["rock", "paper", "scissors"]
+        const pick = ["rock", "paper", "scissors"]
+        const pick2 = ["rock", "paper", "scissors"]
+            const results = pick[Math.floor(Math.floor() * (pick.length))]
+            const results2 = pick2[Math.floor(Math.random() * (pick2.length))]
             let winner = ``
             const person = message.author;
             const person2 = message.mentions.members.first();
@@ -17,8 +19,8 @@ module.exports = {
             if(results[0] && results2[1]) winner = `${person2.user.username}`;
             if(results[1] && results2[0]) winner = `${person.username}`;
             if(results[2] && results2[1]) winner = `${person.username}`;
-            //if(results[])
+            if(results[1] && results2[2]) winner = `${person2.user.username}`;
             if(results2 == results) winner = `You tied with ${person2.user.username}!`;
-            message.channel.send(`${person.tag} vs ${person2.user.tag}\n${winner} won!`)
+            message.channel.send(`${person.tag} vs ${person2.user.tag}\n${person.tag} chose ${results} and ${person2} chose ${results2}\n${winner} won!`)
   }
 }
