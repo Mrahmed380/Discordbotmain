1ba0e434fa87dde7282a4f4b8ab48932327095b9
SupremeERG <ethang717163@icloud.com>
2020-08-01 23:05:27 -0500

    Update rock.paper.sissors.shoot.js

--- commands/entertainment/rock.paper.sissors.shoot.js
+++ commands/entertainment/rock.paper.sissors.shoot.js
@@ -16,10 +16,10 @@ module.exports = {
             const person2 = message.mentions.members.first();
             console.log(`results = ${results} and ${results2}`)
             if (!person2) return message.channel.send("You need to mention the person you want to play with");
-            if(results[0] && results2[1]) winner = `${person2.user.username}`;
-            if(results[1] && results2[0]) winner = `${person.username}`;
-            if(results[2] && results2[1]) winner = `${person.username}`;
-            if(results[1] && results2[2]) winner = `${person2.user.username}`;
+            if(results = "rock" && results2 == "paper") winner = `${person2.user.username}`;
+            if(results == "paper" && results2 == "rock") winner = `${person.username}`;
+            //if(results[2] && results2[1]) winner = `${person.username}`;
+            //if(results[1] && results2[2]) winner = `${person2.user.username}`;
             if(results2 == results) winner = `You tied with ${person2.user.username}!`;
             message.channel.send(`${person.tag} vs ${person2.user.tag}\n${person.tag} chose ${results} and ${person2} chose ${results2}\n${winner} won!`)
   }
