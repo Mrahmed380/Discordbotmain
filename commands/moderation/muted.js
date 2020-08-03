263667c3bc5736c8c73a3d8a46b6073f7397391c
SupremeERG <ethang717163@icloud.com>
2020-08-02 22:07:36 -0500

    Update muted.js

--- commands/moderation/muted.js
+++ commands/moderation/muted.js
@@ -1,14 +1,14 @@
 const { MessaeEmbed, GuildMember, Collection } = require('discord.js')
 module.exports = {
     name: 'muted',
-    desription: 'Shows all muted users',
+    description: 'Shows all muted users',
     usage: 'muted',
     category: 'moderation',
     perms: 'Send Messages',
     run: async (bot, message, args) => {
         const hb = require('hastebin-generator')
-        const modrole = message.guild.roles.cache.find(r => r.name === 'Muted')
-        var formattedMsgs = modrole.members.map(m => `${m.user.tag},\n`)
+        const muterole = message.guild.roles.cache.find(r => r.name === 'Muted')
+        var formattedMsgs = muterole.members.map(m => `${m.user.tag},\n`)
         hb(formattedMsgs.join(""), 'js').then(r => {
 
             message.channel.send(`Here is a list of all the muted users! ${r}`)
