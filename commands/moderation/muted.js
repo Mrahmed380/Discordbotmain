c4574b8dc8d06fd9a5d260480e0aa504585d55b5
SupremeERG <ethang717163@icloud.com>
2020-08-02 21:37:24 -0500

    Fixed spacing errors and added ticket fields

--- commands/moderation/muted.js
+++ commands/moderation/muted.js
@@ -1,19 +1,19 @@
 const { MessaeEmbed, GuildMember, Collection } = require('discord.js')
-module.exports={
+module.exports = {
     name: 'muted',
     desription: 'Shows all muted users',
     usage: 'muted',
     category: 'moderation',
     perms: 'Send Messages',
-    run: async (bot,message,args)=>{
+    run: async (bot, message, args) => {
         const hb = require('hastebin-generator')
         const modrole = message.guild.roles.cache.find(r => r.name === 'Muted')
-            var formattedMsgs = modrole.members.map(m => `${m.user.tag},\n`)
-            hb(formattedMsgs.join(""), 'js').then(r => {
-                
-                message.channel.send(`Here is a list of all the muted users! ${r}`)
-            }).catch(e => {
-                if(e) return;
-            })
+        var formattedMsgs = modrole.members.map(m => `${m.user.tag},\n`)
+        hb(formattedMsgs.join(""), 'js').then(r => {
+
+            message.channel.send(`Here is a list of all the muted users! ${r}`)
+        }).catch(e => {
+            if (e) return;
+        })
     }
 }
