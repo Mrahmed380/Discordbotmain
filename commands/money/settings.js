a275fa3fcbd9015cf20d79f70cb6d268722a168f
SupremeERG <ethang717163@icloud.com>
2020-08-03 01:18:26 -0500

    Update settings.js

--- commands/money/settings.js
+++ commands/money/settings.js
@@ -14,10 +14,10 @@ module.exports = {
     run: async (bot, message, args) => {
         let settings = new MessageEmbed()
             .setTitle("Available settings")
-            .setDescription(`Passive mode: true/false || Users can not rob you in while you are in passive mode\nNotifications: true/false`)
+            .setDescription(`Passive mode: true/false || Users can not rob you in while you are in passive mode\nNotifications: true/false || You will not be DM'ed from the bot if this option is false`)
         let sett = args[0];
         let Switch = args.slice(1).join(" ");
-        let setting = "passive" || "Passive" || "notifications" || "notis";
+        let setting = "passive" ?? "Passive" ?? "notifications" ?? "notis";
         if (!sett) return message.channel.send('Usage: settings <settingName> <true/false>'); console.log(sett)
         if (!Switch) return message.channel.send('Usage: settings <settingName> <true/false>')
         if (sett !== setting) return message.channel.send(settings);
