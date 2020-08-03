e17428e6bf340442e85c90486c25bf87c630b0d0
SupremeERG <ethang717163@icloud.com>
2020-08-02 21:13:38 -0500

    Update prefix.config.js

--- commands/utilities/prefix.config.js
+++ commands/utilities/prefix.config.js
@@ -48,8 +48,8 @@ module.exports = {
                 if (message.content.includes(":")) {
                     const str = newpre
                     const numberOfMatches = (str.match(/:/gu) || []).length;
-                    
-                    console.log((str.test(/porn/)));
+                    let test = (str.test(/porn/))
+                    console.log(test);
                     if(str === ":") return message.channel.send('You prefix needs to contain at least 1 character!')
                     if(numberOfMatches > 1) return message.channel.send("You can only put one space in a prefix!");
                     const res = str.replace(":", " ");
